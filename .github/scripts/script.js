8a8d522dd26023ad8a395bd1742d8aebbfbc5615
Chan Nyein Ko Ko <87624949+ohcnkk@users.noreply.github.com>
2022-08-27 06:43:45 +0000

    script file

--- .github/scripts/script.js
+++ .github/scripts/script.js
@@ -1,3 +1,17 @@
+const http = require('@actions/http-client');
+
+const base = new URL('https://httpbin.org/');
+
+async function realVersion() {
+    const latest = new URL(`/get`, base)
+    const client = new http.HttpClient('', '', { allowRedirects: true });
+    const res = await client.head(latest);
+
+    console.log(res.message.statusCode)
+}
+
+realVersion()
+
 module.exports = () => {
-    return 'fooo'
+    return res.message.statusCode
 }
